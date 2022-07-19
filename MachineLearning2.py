f0f6abbf010424e276855d7f03b1eca72a86bbe0
cesar214567 <camgcamg11@gmail.com>
2022-07-19 14:19:48 -0500

    added hamming score

--- MachineLearning2.py
+++ MachineLearning2.py
@@ -10,6 +10,7 @@ from keras.layers import Dropout
 import os
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import multilabel_confusion_matrix
+from sklearn.metrics import classification_report
 
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 
@@ -36,11 +37,32 @@ def get_model_MLP(n_inputs, n_outputs):
 	model.compile(loss='binary_crossentropy', optimizer='adam')
 	return model
 
+def hamming_score(y_true, y_pred, normalize=True, sample_weight=None):
+    '''
+    Compute the Hamming score (a.k.a. label-based accuracy) for the multi-label case
+    http://stackoverflow.com/q/32239577/395857
+    '''
+    acc_list = []
+    for i in range(y_true.shape[0]):
+        set_true = set( np.where(y_true[i])[0] )
+        set_pred = set( np.where(y_pred[i])[0] )
+        #print('\nset_true: {0}'.format(set_true))
+        #print('set_pred: {0}'.format(set_pred))
+        tmp_a = None
+        if len(set_true) == 0 and len(set_pred) == 0:
+            tmp_a = 1
+        else:
+            tmp_a = len(set_true.intersection(set_pred))/\
+                    float( len(set_true.union(set_pred)) )
+        #print('tmp_a: {0}'.format(tmp_a))
+        acc_list.append(tmp_a)
+    return np.mean(acc_list)
+
 def get_model_MultiOutput_Logistic():
     return MultiOutputClassifier(LogisticRegression())
 
 def get_model_MultiOutput_SGDC():
-    return MultiOutputClassifier(SGDClassifier(max_iter=10000, tol=1e-3,verbose = 0))
+    return MultiOutputClassifier(SGDClassifier(max_iter=10000, tol=1e-5,verbose = 0))
 
 
 def train_model(X, y,model = 'mlp'):
@@ -74,18 +96,27 @@ def test_model(model,X_test,y_test):
 	print(results_table)
 	return results
 
+
+emotionLabels = ["TRU","DIS","JOY","SAD","ANT","SUR","ANG","FEA","NEUTRAL"]
+
 model = train_model(X_train,Y_train,'mlp')
 results = test_model(model,X_test,Y_test)
 print('Acurácia: ', accuracy_score(Y_test, results, normalize=True), '%')
+print('hamming score: ', hamming_score(Y_test, results), '%')
 print(multilabel_confusion_matrix(Y_test,results))
+print(classification_report(Y_test, results, target_names=emotionLabels))
 
 model = train_model(X_train,Y_train,'MultiOutput_Logistic')
 results = test_model(model,X_test,Y_test)
 print('Acurácia: ', accuracy_score(Y_test, results, normalize=True), '%')
+print('hamming score: ', hamming_score(Y_test, results), '%')
 print(multilabel_confusion_matrix(Y_test,results))
+print(classification_report(Y_test, results, target_names=emotionLabels))
 
 model = train_model(X_train,Y_train,'MultiOutput_SGDC')
 results = test_model(model,X_test,Y_test)
 print('Acurácia: ', accuracy_score(Y_test, results, normalize=True), '%')
+print('hamming score: ', hamming_score(Y_test, results), '%')
 print(multilabel_confusion_matrix(Y_test,results))
+print(classification_report(Y_test, results, target_names=emotionLabels))
 
